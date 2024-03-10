d39ec5882b3915c1c5ee2bd298b774dc6eb0d784
Bojan Milosevic <bokiledobri@gmail.com>
2024-03-10 19:03:26 +0100

    fixed functions checking false logic

--- asserts.go
+++ asserts.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-//Assert simply checks if given statement is true, and if not, calls t.Errorf with message and msgArgs aas parameters
+//Assert  calls t.Errorf with message and msgArgs aas parameters if statement is false
 func Assert(t testing.TB, statement bool, message string, msgArgs ...any) {
 	if !statement {
 		t.Errorf(message, msgArgs...)
@@ -16,29 +16,29 @@ func Assert(t testing.TB, statement bool, message string, msgArgs ...any) {
 //AssertErrNotNil fails test with err.Error print if err is not nil
 func AssertErrNotNil(t testing.TB, err error) {
 	t.Helper()
-	Assert(t, err != nil, "expected error to be nil, got:\n %v", err)
+	Assert(t, err == nil, "expected error to be nil, got:\n %v", err)
 }
 //AssertBody ails the test if body of resp  does not contain want
 func AssertBody(t testing.TB, resp *http.Response, want string) {
 	t.Helper()
 	body, err := io.ReadAll(resp.Body)
 	AssertErrNotNil(t, err)
-	Assert(t, !(strings.Contains(string(body), want)), "expected request body to contain %s, got: \n %s", want, body)
+	Assert(t, strings.Contains(string(body), want), "expected request body to contain %s, got: \n %s", want, body)
 }
 
 //AssertStatus fails the test if status code of resp is not equal to want
 func AssertStatus(t testing.TB, resp *http.Response, want int) {
 	t.Helper()
 	s := resp.StatusCode
-	Assert(t, want != s, "expected status of %d, got %d", want, s)
+	Assert(t, want == s, "expected status of %d, got %d", want, s)
 }
 //AssertCookieExists fails the test if cookie with given name does/does not exist depending on exists parameter
 func AssertCookieExists(t testing.TB, resp *http.Response, name string, exists bool) {
     _, e := getCookieValue(resp, "jwt")
 	if exists {
-		Assert(t, !e, "Expected %s name cookie to exist, but it doesn't", name)
+		Assert(t, e, "Expected %s name cookie to exist, but it doesn't", name)
 	} else {
-		Assert(t, e, "Did not  expect %s name cookie to exist, but it does", name)
+		Assert(t, !e, "Did not  expect %s name cookie to exist, but it does", name)
 	}
 }
 
