2fe8b4a1ca1dc9cdd9e968177a003c3632909260
Bojan Milosevic <bokiledobri@gmail.com>
2024-03-11 17:09:06 +0100

    Exported GetCookieValue, replaced AssertErrNotNil with AssertErrNil(exists bool) and commented exported methods

--- asserts.go
+++ asserts.go
@@ -14,15 +14,20 @@ func Assert(t testing.TB, statement bool, message string, msgArgs ...any) {
 	}
 }
 //AssertErrNotNil fails test with err.Error print if err is not nil
-func AssertErrNotNil(t testing.TB, err error) {
+func AssertErrNil(t testing.TB, err error, exists bool) {
 	t.Helper()
+    if !exists{
 	Assert(t, err == nil, "expected error to be nil, got:\n %v", err)
+}else{
+
+	Assert(t, err != nil, "expected error to exist, got nil")
+}
 }
 //AssertBody fails test if body of resp  does not contain want
 func AssertBody(t testing.TB, resp *http.Response, want string) {
 	t.Helper()
 	body, err := io.ReadAll(resp.Body)
-	AssertErrNotNil(t, err)
+	AssertErrNil(t, err, false)
 	Assert(t, strings.Contains(string(body), want), "expected request body to contain %s, got: \n %s", want, body)
 }
 
@@ -34,7 +39,8 @@ func AssertStatus(t testing.TB, resp *http.Response, want int) {
 }
 //AssertCookieExists fails test if cookie with given name does/does not exist depending on exists parameter
 func AssertCookieExists(t testing.TB, resp *http.Response, name string, exists bool) {
-    _, e := getCookieValue(resp, "jwt")
+    t.Helper()
+    _, e := GetCookieValue(resp, "jwt")
 	if exists {
 		Assert(t, e, "Expected %s name cookie to exist, but it doesn't", name)
 	} else {
@@ -42,7 +48,8 @@ func AssertCookieExists(t testing.TB, resp *http.Response, name string, exists b
 	}
 }
 
-func getCookieValue(resp *http.Response, cookieName string) (value string, exists bool) {
+//GetCookieValue returns value of cookie named cookieName and if it exists 
+func GetCookieValue(resp *http.Response, cookieName string) (value string, exists bool) {
 	cookies := resp.Cookies()
 	for _, c := range cookies {
 		if c.Name == cookieName {
--- tests.go
+++ tests.go
@@ -7,34 +7,39 @@ import (
 	"testing"
 )
 
-	type TestData struct {
-		Request            interface{}
-		Route              string
-		ExpectedStatusCode int
-		ExpectedBody       string
-		ExpectedCookie     bool
-		Description        string
-		Headers map[string]string
-	}
+//TestData are common fields for http request and expected values
+type TestData struct {
+	Request            interface{}
+	Route              string
+	ExpectedStatusCode int
+	ExpectedBody       string
+	ExpectedCookie     bool
+	Description        string
+	Headers            map[string]string
+    ErrNil bool
+}
 
-	func (test *TestData) SetupRequest() (*http.Request, error){
-			req, err := http.NewRequest(http.MethodPost, test.Route, parseToJSON(test.Request))
-			for k, v := range test.Headers{
-				req.Header.Add(k, v)
-			}
-			return req, err
-		}
-	func (test *TestData) Run(t *testing.T, res *http.Response, err error){
-		t.Helper()
-		
-		t.Run(test.Description, func(t *testing.T) {
-            t.Log(parseToJSON(test.Request).String())
-            AssertErrNotNil(t, err)
-			AssertStatus(t, res, test.ExpectedStatusCode)
-			AssertCookieExists(t, res, "jwt", test.ExpectedCookie)
-			AssertBody(t, res, test.ExpectedBody)
-		})
+//SetupRequest creates *http.Request with provided fields in TestData
+func (test *TestData) SetupRequest() (*http.Request, error) {
+	req, err := http.NewRequest(http.MethodPost, test.Route, parseToJSON(test.Request))
+	for k, v := range test.Headers {
+		req.Header.Add(k, v)
 	}
+	return req, err
+}
+
+//Run takes *http.Response and possible error and asserts error, cookie, status and body with TestData expected fields
+func (test *TestData) Run(t *testing.T, res *http.Response, err error) {
+	t.Helper()
+
+	t.Run(test.Description, func(t *testing.T) {
+		t.Log(parseToJSON(test.Request).String())
+		AssertErrNil(t, err, test.ErrNil)
+		AssertStatus(t, res, test.ExpectedStatusCode)
+		AssertCookieExists(t, res, "jwt", test.ExpectedCookie)
+		AssertBody(t, res, test.ExpectedBody)
+	})
+}
 func parseToJSON(o interface{}) *bytes.Buffer {
 	buffer := bytes.NewBuffer([]byte{})
 	encoder := json.NewEncoder(buffer)
